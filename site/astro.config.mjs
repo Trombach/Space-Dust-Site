585b2065a4e384bff9241144b801bf40d5c0f04a
Samrridh Khanna <samrridh.code@gmail.com>
2024-08-02 00:17:11 +0530

    published on netlify and vercel, going to on cf to check speed

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-
 import mdx from "@astrojs/mdx";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), mdx()]
+  integrations: [tailwind(), sitemap(), mdx(), partytown()]
 });
