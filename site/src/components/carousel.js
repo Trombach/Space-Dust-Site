c12d58f72dc6261b95cb18b59d6d88b0b6f7df3e
Samrridh Khanna <samrridh.code@gmail.com>
2024-08-03 00:00:56 +0530

    created index page carousel styling about page remaining

--- site/src/components/carousel.js
+++
@@ -1,8 +0,0 @@
-const images = document.querySelectorAll('[data-carousel-index]');
-let currentIndex = 0;
-
-setInterval(() => {
-  images[currentIndex].style.opacity = '0';
-  currentIndex = (currentIndex + 1) % images.length;
-  images[currentIndex].style.opacity = '1';
-}, 5000);
