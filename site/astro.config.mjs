621129622240bdc4988a70d79bec8b29310ff5ed
Samrridh Khanna <samrridh.code@gmail.com>
2024-08-16 23:41:40 +0530

    added alpine.js moved blog from content to pages folder config.ts has caused problem in env.d.ts, check when you can understand the problem added eslint

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
-
 import partytown from "@astrojs/partytown";
+
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), mdx(), partytown()]
+  site: 'https://spacedust.in',
+  integrations: [tailwind(), sitemap(), mdx(), partytown(), alpinejs()]
 });
